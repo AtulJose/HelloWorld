2716ba40270e09dd29c50ff878d5aa485a60ee90
atuljose <atulpj@gmail.com>
2020-06-21 08:06:25 -0600

    Added fistName and Last Name

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,8 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Atul";
-		System.out.println("Hello: " + name);
+		String firstName = "Kathy";
+		String lastName = "Jacob";
+		System.out.println("Hello: " + firstName + " "+ lastName);
 		System.out.println("How can I help you??");
 
 	}
