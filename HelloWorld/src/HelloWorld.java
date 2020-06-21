a02167fde02c06214c4ba5fa0a85f2e8af0e9bd3
atuljose <atulpj@gmail.com>
2020-06-21 08:08:43 -0600

    Changed Name

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Atul";
+		String name = "Joe";
 		System.out.println("Hello: " + name);
 		System.out.println("How can I help you??");
 
