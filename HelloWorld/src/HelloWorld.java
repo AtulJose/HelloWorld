56e8393eef70616cdc629032946b8d6c40f60e2a
atulp <atulp@LAPTOP-SCIK7NH6>
2020-06-21 07:42:12 -0600

    Display name

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		String name = "Atul";
+		System.out.println("Hello: " + name);
 		System.out.println("How can I help you??");
 
 	}
