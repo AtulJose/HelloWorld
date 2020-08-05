3ed27375eaaedb01c7c48048b1cc481fb806c393
atuljose <atulpj@gmail.com>
2020-08-05 12:57:39 -0600

    test commit after squash

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,9 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< Upstream, based on dae78c36a9827d5e87880849cf251b24a5d3bcf3
 		String firstName = "Kathy";
 		String lastName = "Jacob";
 		System.out.println("Hello: " + firstName + " "+ lastName);
+=======
+		String name = "Joe";
+		System.out.println("Hello: " + name);
+>>>>>>> a02167f Changed Name
 		System.out.println("How can I help you??");
 
 	}
