ce8f897527b0f8e3657f7838409843e25c300b18
atuljose <atulpj@gmail.com>
2020-08-05 13:08:16 -0600

    Changes made in  Test Hello branch

---
+++ HelloWorld/src/HelloAtul.java
@@ -0,0 +1,12 @@
+
+public class HelloAtul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Atul";
+		System.out.println("Hello: " + name);
+		System.out.println("How can I help you??");
+
+	}
+
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,14 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		String name = "Joe";
+		String name = "Ryan";
 		System.out.println("Hello: " + name);
-=======
 		String firstName = "Kathy";
 		String lastName = "Jacob";
 		System.out.println("Hello: " + firstName + " "+ lastName);
->>>>>>> d0c6d19835f0b1eea08b832883e1565db3d58145
+
 		System.out.println("How can I help you??");
 
 	}
