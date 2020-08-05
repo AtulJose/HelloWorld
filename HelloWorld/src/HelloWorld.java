91ba1272c4c92a6af7cbed103d4fb54c37b52b39
atuljose <atulpj@gmail.com>
2020-08-05 13:29:39 -0600

    eclipse commit 4

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String firstName = "Atul";
-		String lastName = "Jose";
+		String firstName = "Kathy";
+		String lastName = "Jacob";
 		System.out.println("Hello: " + firstName + " "+ lastName);
 		System.out.println("How can I help you??");
 
